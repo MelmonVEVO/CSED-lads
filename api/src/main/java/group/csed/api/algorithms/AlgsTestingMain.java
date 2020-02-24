e41602a0d388291b5dfa42c0dd0f39d3b7326e40
Melmon <dylan.drescher@gmail.com>
2020-02-24 18:16:30 +0000

    Added new algorithm skeleton files

---
+++ api/src/main/java/group/csed/api/algorithms/AlgsTestingMain.java
@@ -0,0 +1,7 @@
+package group.csed.api.algorithms;
+
+public class AlgsTestingMain {
+	public static void AlgsTestingMain(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
---
+++ api/src/main/java/group/csed/api/algorithms/NextPrediction.java
@@ -0,0 +1,5 @@
+package group.csed.api.algorithms;
+
+public class NextPrediction {
+	NextPrediction() { }
+}
