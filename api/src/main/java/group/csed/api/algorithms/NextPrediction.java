98fc5d8e0fe39ea0efded59578baacb921ce1eda
the-rhodri <61012799+the-rhodri@users.noreply.github.com>
2020-02-27 18:11:11 +0000

    Update NextPrediction.java

--- api/src/main/java/group/csed/api/algorithms/NextPrediction.java
+++ api/src/main/java/group/csed/api/algorithms/NextPrediction.java
@@ -1,5 +1,11 @@
 package src.main.java.group.csed.api.algorithms;
 
 public class NextPrediction {
-	NextPrediction() { }
+
+    public static Date nextPrediction(Date previousPeriod, int cycleLen){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(previousPeriod);
+        cal.add(Calendar.DATE, cycleLen);
+        return cal.getTime();
+    }
 }
