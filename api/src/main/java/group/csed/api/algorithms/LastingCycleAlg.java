e831683aa38166ec92a06697edc5aea401d7dff2
Melmon <dylan.drescher@gmail.com>
2020-03-05 12:49:38 +0000

    Added the lasting cycle algorithm and imported needed classes in NextPrediction

--- api/src/main/java/group/csed/api/algorithms/LastingCycleAlg.java
+++ api/src/main/java/group/csed/api/algorithms/LastingCycleAlg.java
@@ -1,5 +1,13 @@
 package group.csed.api.algorithms;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class LastingCycleAlg {
-	LastingCycleAlg() { }
+	public static double getLengthOfCycle(Date startDate, Date endDate) {
+		long sDateMili = startDate.getTime();
+		long eDateMili = endDate.getTime();
+		long miliDateDiff = eDateMili - sDateMili;
+		return miliDateDiff / (8.64 * Math.pow(10, 7));
+	}
 }
--- api/src/main/java/group/csed/api/algorithms/NextPrediction.java
+++ api/src/main/java/group/csed/api/algorithms/NextPrediction.java
@@ -1,5 +1,8 @@
 package group.csed.api.algorithms;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class NextPrediction {
 
     public static Date nextPrediction(Date previousPeriod, int cycleLen){
