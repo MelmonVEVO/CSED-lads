f9f27685cef50b6f4362e0619564275bb1cbff14
Henry Eason <henryeason50@gmail.com>
2020-03-15 18:54:07 +0000

    Began work on console UI

--- frontend/src/main/java/group/csed/frontend/Frontend.java
+++ frontend/src/main/java/group/csed/frontend/Frontend.java
@@ -13,18 +13,35 @@ public class Frontend {
         presentLoginPage();
     }
 
+    private void presentMenu() {
+        System.out.println("------- Main menu -------");
+        System.out.println("1. Update period data");
+    }
+
+    private void getMainMenuInput() {
+        String input;
+        while(true) {
+            presentMenu();
+            input = consoleReader.readLine();
+        }
+    }
+
     private void presentLoginPage() {
         String email, password;
-        email = getLoginInput("Please enter your email");
-        password = getLoginInput("Please enter your password");
+        while(true) {
+            email = getLoginInput("Please enter your email");
+            password = getLoginInput("Please enter your password");
 
-        ApiRequest.login(email, password, (status, id) -> {
-            if(status == Status.OK) {
-                System.out.println("Successfully logged in");
-            } else {
-                System.out.println("Incorrect email or password");
-            }
-        });
+            ApiRequest.login(email, password, (status, id) -> {
+                if(status == Status.OK) {
+                    System.out.println("Successfully logged in");
+                    getMainMenuInput();
+                    return;
+                } else {
+                    System.out.println("Incorrect email or password");
+                }
+            });
+        }
     }
 
     private String getLoginInput(String message) {
