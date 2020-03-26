4b588be2a62456c58591b0f16805ba00e57b1378
Henry Eason <henryeason50@gmail.com>
2020-03-26 13:42:20 +0000

    Fixed prediction

--- frontend/src/main/java/group/csed/frontend/predictions/PeriodPrediction.java
+++ frontend/src/main/java/group/csed/frontend/predictions/PeriodPrediction.java
@@ -5,17 +5,29 @@ import java.util.Date;
 
 public class PeriodPrediction {
 
+    private static final int[] monthLens = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    private static Date addDays(Date previousDate, int daysToAdd) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(previousDate);
+        int monthLen = monthLens[cal.get(Calendar.MONTH)];
+        if(cal.get(Calendar.DAY_OF_MONTH) + daysToAdd > monthLen){
+            daysToAdd = cal.get(Calendar.DAY_OF_MONTH) + daysToAdd - monthLen - 1;
+            cal.set(Calendar.MONTH, cal.get(Calendar.MONTH) + 1);
+            cal.set(Calendar.DAY_OF_MONTH, 1);
+        }
+        cal.add(Calendar.DATE, daysToAdd);
+        return cal.getTime();
+    }
+
+    public static Date getNextPeriodDate(Date previousPeriod, int cycleLen) {
+        return addDays(previousPeriod, cycleLen);
+    }
+
     public static double getLengthOfCycle(Date startDate, Date endDate) {
         long startTime = startDate.getTime();
         long endTime = endDate.getTime();
         long timeDiff = endTime - startTime;
         return timeDiff / (8.64 * Math.pow(10, 7));
-    }
-
-    public static Date getNextPeriodDate(Date previousPeriod, int cycleLen) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(previousPeriod);
-        cal.add(Calendar.DATE, cycleLen);
-        return cal.getTime();
     }
 }
